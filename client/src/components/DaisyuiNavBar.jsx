9d83f74403d5e39437315f53201069f12384c8b4
cragone <ragonecharlie@gmail.com>
2024-03-08 11:12:28 -0500

    can route to regeneron home

--- client/src/components/DaisyuiNavBar.jsx
+++ client/src/components/DaisyuiNavBar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Regeneron</a>
+        <a href="https://ron.regeneron.com/home" className="btn btn-ghost text-xl">Regeneron</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
